8030e10576c8aa013c0ad07f39588caf9f17ae08
sksultanov <skaish@yandex.ru>
2018-03-28 16:05:36 +0500

    Оживил форму

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+
+
+var link = document.querySelector(".button-interesting");
+var popup = document.querySelector(".interesting-form");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("form-show");
+});
+
+   
