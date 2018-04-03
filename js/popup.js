c96a9ab356e6fd9158b0b7887a7a9aedca8df109
sksultanov <skaish@yandex.ru>
2018-04-03 21:39:15 +0500

    Анимировал форму

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,26 @@
-
-
 var link = document.querySelector(".button-interesting");
 var popup = document.querySelector(".interesting-form");
+
+var race = popup.querySelector("[name=date-race]");
+var form = popup.querySelector("form");
+var departure = popup.querySelector("[name=date-departure]");
+
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
+    popup.classList.remove("modal-error");
     popup.classList.toggle("form-show");
 });
+
+form.addEventListener("submit", function (evt) {
+  if (!race.value || !departure.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+
+  }
+});
+
+
 
 
